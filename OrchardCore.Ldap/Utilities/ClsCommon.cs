3a54d0dc6c5f8fe372cbf9688892d348c081f4f1
NgoTuanTruong <tringochinh@gmail.com>
2021-10-02 15:36:03 +0700

    Update

--- OrchardCore.Ldap/Utilities/ClsCommon.cs
+++ OrchardCore.Ldap/Utilities/ClsCommon.cs
@@ -4,12 +4,11 @@ namespace OrchardCore.Ldap.Utilities
 {
     public static class ClsCommon
     {
-        public const string MethodPost = "POST";
         public const string RequestLogin = @"/Login";
         public static bool IsRequestLogin(HttpContext context)
         {
             if(context.Request.Path.StartsWithSegments(RequestLogin)
-                && context.Request.Method.ToUpper() == MethodPost)
+                && HttpMethods.IsPost(context.Request.Method))
             {
                 return true;
             }
